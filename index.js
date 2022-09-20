aefe06e2f3663ccdf90bac13e360923ab0e6f59c
Phil415 <phillevy415@gmail.com>
2022-09-20 10:50:56 -0700

    Button Clicker First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function displayName(elementName) {
+    console.log(elementName)
+    elementName.innerText = "Logout";
+}
+
+
+var count = 1;
+var countElement = document.querySelector("#count")
+function add1() {
+    count++;
+    countElement.innerText = "the count is" + count;
+    console.log(count);
+}
